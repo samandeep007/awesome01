5a44799116b0190d2a3f65418eafee0f482c0966
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-02 18:38:22 -0600

    chore: Update babel.config.js and index.js
    The changes in babel.config.js update the presets array to use "@react-native/babel-preset" for compiling React Native code. The addition of a comment explains the purpose of the file.
    
    In index.js, a comment is added to explain the purpose of the file and its relation to package.json.
    
    ```

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
 };
+ // We have babel.config.js in the root of our project directory. This file tells Babel how to compile our code. The presets array tells Babel to use the "@react-native/babel-preset" which is a preset for React Native projects.
--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
+// This is where all the magic happens
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+//All the namings and configurations for the app is written in package.json
