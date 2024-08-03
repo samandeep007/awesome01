16108c076634cc4d9d5420bd6f2d42b1570e9231
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-03 03:08:58 -0600

    chore: Update AppPro component to use color scheme for text color

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,37 +1,28 @@
-import React from 'react'
-import {
-    View,
-    Text,
-    StyleSheet,
-    useColorScheme
-} from 'react-native'
+import React from 'react';
+import {View, Text, StyleSheet, useColorScheme} from 'react-native';
 
 export default function AppPro(): JSX.Element {
 
-    const isDarkMode = useColorScheme() === 'dark'
-
-
+    const darkMode = useColorScheme() === "dark";
     return (
         <View style={styles.container}>
-            <Text style={isDarkMode ? styles.darkText : styles.lightText}>Samandeep Singh</Text>
+            <Text style={darkMode ? styles.darkText : styles.whiteText}>My name is Samandeep Singh Sandhu</Text>
         </View>
     )
 }
 
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center'
+        alignItems: "center", //Changes things on the x-axis
+        justifyContent: "center" // On y-axis
     },
 
     darkText: {
-        color: "white"
+        color:'#FFFFFF'
     },
 
-    lightText: {
-        color: "red"
+    whiteText: {
+        color: '#000000'
     }
-
-
 })
