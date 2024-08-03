a5d1242d45c5cb0af03bef3fe8d8935210ff79af
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-02 19:07:43 -0600

    chore: Update App.tsx to improve UI and add explanatory comments

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {
-     Text
-} from 'react-native';
+import { Text, View, Alert, Button, SafeAreaView } from 'react-native';
 
-
-export default function App(){
-
+export default function App() {
   return (
-  <>
-<Text style={{color: "yellow", fontSize: 50}}>Samandeep Singh</Text>
-  </>
+    <SafeAreaView>
+      <View>
+        <Text style={{ color: "pink", fontSize: 30, padding: 20 }}>Let's learn about React Native together</Text>
+        <Text style={{marginBottom: 20, fontSize: 20, padding: 20}}>React-native is very similar to the react that we have for web development, the only difference is that react native uses JSX elements of its own to render the things to the activity.
+          While react uses HTML. View tag is used as alternative to HTML div. Everything is wrapped inside these views, and these views are further wrapped within a single element called SafeAreaView which is responsible for handling the notches that most of the latest phones have
+        </Text>
+        <Button color={"red"}  title="Hello" onPress={() => Alert.alert("Hello", "Kii haal ne")}></Button>
+      </View>
+    </SafeAreaView>
   )
 }
-
-
